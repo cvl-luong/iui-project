f246eb96854f8b27b041a6790a07274fc1a5cb47
Long Luong <cvl.luong@student.maastrichtuniversity.nl>
2024-11-26 15:18:42 +0100

    e

---
+++ hard-coded.md
@@ -0,0 +1,11 @@
+Imagine you come home from a long day of work and you need to prepare dinner this Thursday evening. You have been using this app for a while now, so this application is aware of your user habits, such as preferences depending on the day of the week, or dietary preferences. You are going to use this application to decide what to have for dinner. 
+
+Here are the steps you need to follow:
+1. On the first page, select "Test Application Without AI"
+
+
+
+Here are
+1. On the first page, select "Test Application With AI"
+A few potions have already been selected for you because the application keeps track of your habits. In this case, the fields "preparation time", "Dietary preferences / allergies", "Portion size" and "kitchen appliances" have already been filled out as such.
+1. A few potions have already been selected for you, but some fields are still missing. Under "Filters", select the checkboxes "Italian" and ""
--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+let ai = false;
+
 document.querySelectorAll('.test-button').forEach(button => {
     button.addEventListener('click', function () {
         console.log(this.innerText);
+        if (this.innerText == "TEST APPLICATION WITH AI") {
+            ai = true;
+        }
+        console.log(ai)
         document.getElementById("button-container").style.display = "none";
+        document.getElementById("main").style.display = "block";
     });
 });
+
+document.querySelectorAll('.category-card').forEach(button => {
+    button.addEventListener('click', function () {
+        this.classList.toggle("highlight");
+    });
+});
+
+document.querySelectorAll('.search-button').forEach(button => {
+    button.addEventListener('click', function () {
+        document.getElementsByClassName("recipe-categories")[0].style.display = "none";
+        console.log(document.getElementsByClassName("recipe-display")[0])
+        document.getElementsByClassName("recipe-display")[0].style.display = "block";
+    });
+});
+
